5ef8afbcf103556e2e7eaae0ec3b199afafd110b
Paul Romero <pdromero@espol.edu.ec>
2022-11-04 01:01:36 -0500

    Prueba cambiando en netbeans

--- Proyecto_1P_Abad_Romero_Soriano/src/main/java/com/mycompany/proyecto_1p_abad_romero_soriano/Proyecto_1P_Abad_Romero_Soriano.java
+++ Proyecto_1P_Abad_Romero_Soriano/src/main/java/com/mycompany/proyecto_1p_abad_romero_soriano/Proyecto_1P_Abad_Romero_Soriano.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.proyecto_1p_abad_romero_soriano;
 
-/**
- *
- * @author Johan
- */
+
 public class Proyecto_1P_Abad_Romero_Soriano {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        
+        
+        
+        
     }
 }
