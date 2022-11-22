c60042d9a3294ddcecb215377ee1838e54be9a26
Paul Romero <pdromero@espol.edu.ec>
2022-11-22 16:36:38 -0500

    Revision del proyecto de lectura de archivos

--- LecturaEscrituraArchivos/src/main/java/manejoArchivos/ManejoArchivos.java
+++ LecturaEscrituraArchivos/src/main/java/manejoArchivos/ManejoArchivos.java
@@ -1,18 +1,9 @@
 package manejoArchivos;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-/**
- *
- * @author Verónica
- */
+
 public class ManejoArchivos {
 
     public static ArrayList<String> LeeFichero(String nombrearchivo) {
--- LecturaEscrituraArchivos/src/main/java/manejoArchivos/TrabajoConArchivos.java
+++ LecturaEscrituraArchivos/src/main/java/manejoArchivos/TrabajoConArchivos.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package manejoArchivos;
 
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- *
- * @author Verónica
- */
+
 public class TrabajoConArchivos {
 
     /**
