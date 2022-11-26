545558b0987af39e9867477eedab69c3df1fbaba
axsl3 <axsorian@espol.edu.ec>
2022-11-26 00:03:32 -0500

    cambio leve

--- Proyecto_1P_Abad_Romero_Soriano/src/main/java/SistemaCompraTicketsAereos/SistemaCompraTicketsAereos.java
+++ Proyecto_1P_Abad_Romero_Soriano/src/main/java/SistemaCompraTicketsAereos/SistemaCompraTicketsAereos.java
@@ -65,7 +65,6 @@ public class SistemaCompraTicketsAereos {
 
         //color do while para repetir este
         System.out.println("1. Comprar tickets aéreos");
-
         System.out.println("2. Consultar reservas");
         System.out.println("3. Salir");
         
